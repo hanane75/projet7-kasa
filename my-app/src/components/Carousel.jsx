280d6cb4a7476a2a72e79adec2eed545ef8b2fd9
SALHI Hanane <benkada.hanane@gmail.com>
2024-07-05 19:46:57 +0200

    mise a jours du carrousel

--- my-app/src/components/Carousel.jsx
+++ my-app/src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 const Carousel = ({ pictures }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -7,22 +8,28 @@ const Carousel = ({ pictures }) => {
     const newIndex = isFirstSlide ? pictures.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const goToNext = () => {
     const isLastSlide = currentIndex === pictures.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
+
   return (
     <div className="carousel">
       <div className="carousel-inner">
         <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="carousel-image" />
-        <div className="carousel-navigation">
-          <img src='flechegauche.png' alt="Previous" onClick={goToPrevious} className="carousel-arrow left-arrow" />
-          <img src='flechedroit.png' alt="Next" onClick={goToNext} className="carousel-arrow right-arrow" />
-        </div>
-        <div className="carousel-counter">
-          {currentIndex + 1} / {pictures.length}
-        </div>
+        {pictures.length > 1 && (
+          <>
+            <div className="carousel-navigation">
+              <img src='flechegauche.png' alt="Previous" onClick={goToPrevious} className="carousel-arrow left-arrow" />
+              <img src='flechedroit.png' alt="Next" onClick={goToNext} className="carousel-arrow right-arrow" />
+            </div>
+            <div className="carousel-counter">
+              {currentIndex + 1} / {pictures.length}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
